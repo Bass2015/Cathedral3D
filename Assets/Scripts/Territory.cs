c140788af1183fa7385a9453ee6ff45ebcd1ef6c
Sebastian <bass.dolg@gmail.com>
2021-05-04 12:19:37 +0200

    Copia inicial, acabo de arreglar la detección de vecindario

---
+++ Assets/Scripts/Territory.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Territory {
+
+	List<Square> squares;
+
+	public List<Square> Squares{
+		get	{
+			return squares;
+		}
+	}
+
+	PlayerData owner;
+
+	public Territory(List<Square> squares, PlayerData owner){
+		this.squares = squares;
+		this.owner = owner;
+	}
+
+	void DebugSquares(){
+		string s = "";
+		foreach (Square sq in squares) {
+			s += sq.ToString() + ", ";		
+		}
+		Debug.Log(s);
+	}
+}
